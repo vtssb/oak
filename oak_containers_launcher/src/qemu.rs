868ed8840fbb4cbdb2325bac36785f8ec67ccec7
Yu Ding <dingelish@google.com>
2024-05-30 16:55:27 -0700

    Enable SEV-SNP in oak_containers_launcher
    This change addes essential QEMU parameters to enable SEV-SNP, uses virtio-net-pci with modern API for networking, and let kernel output more logs for debugging. All of the new parameters come from AMD's testing instructions [1].
    
    [1] https://lore.kernel.org/kvm/20240502231140.GC13783@ls.amr.corp.intel.com/T/
    
    Change-Id: I7012e4a47410d6dc6472979af8840c7af997c211

--- oak_containers_launcher/src/qemu.rs
+++ oak_containers_launcher/src/qemu.rs
@@ -141,7 +141,9 @@ impl Qemu {
         // restart should be treated as a failure)
         cmd.arg("-no-reboot");
         // Use the `microvm` machine as the basis, and ensure ACPI and PCIe are enabled.
-        cmd.args(["-machine", "microvm,acpi=on,pcie=on"]);
+        cmd.args(["-machine", "microvm,acpi=on,pcie=on,confidential-guest-support=sev0,memory-backend=ram1"]);
+        cmd.args(["-object", "memory-backend-memfd,id=ram1,size=8G,share=true,reserve=false"]);
+        cmd.args(["-object", "sev-snp-guest,id=sev0,cbitpos=51,reduced-phys-bits=1,id-auth="]);
         // Route first serial port to console.
         if let Some(port) = params.telnet_console {
             cmd.args(["-serial", format!("telnet:localhost:{port},server").as_str()]);
@@ -173,7 +175,7 @@ impl Qemu {
             "hostfwd=tcp:{host_address}:50051-{vm_address}:50051"
         ));
         cmd.args(["-netdev", netdev_rules.join(",").as_str()]);
-        cmd.args(["-device", "virtio-net,netdev=netdev,rombar=0"]);
+        cmd.args(["-device", "virtio-net-pci,disable-legacy=on,iommu_platform=true,netdev=netdev,romfile="]);
         if let Some(virtio_guest_cid) = params.virtio_guest_cid {
             cmd.args([
                 "-device",
@@ -199,7 +201,6 @@ impl Qemu {
                 format!("brd.rd_size={ramdrive_size}").as_str(),
                 "brd.max_part=1",
                 format!("ip={vm_address}:::255.255.255.0::eth0:off").as_str(),
-                "quiet",
             ]
             .join(" ")
             .as_str(),
